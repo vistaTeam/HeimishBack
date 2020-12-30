f94254151447b790ee5476ce1e7edb9727240107
Mendy Joseph <68554280+vistaTeam@users.noreply.github.com>
2020-12-30 20:08:26 +0200

    gg

--- Client/src/app/main/apps/calendar/CalendarApp.js
+++ Client/src/app/main/apps/calendar/CalendarApp.js
@@ -230,7 +230,7 @@ function CalendarApp(props) {
 				resizable
 				onEventResize={resizeEvent}
 				defaultView={Views.MONTH}
-				defaultDate={new Date(2020, 3, 1)}
+				defaultDate={new Date()}
 				startAccessor="start"
 				endAccessor="end"
 				views={allViews}
